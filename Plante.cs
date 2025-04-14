9140da7d4593df6748f5bb0b16fcb53a4b364d47
Emma <eveyrent@ensc.fr>
2025-04-14 17:10:38 +0200

    ajout d'explications

--- Plante.cs
+++ Plante.cs
@@ -1,20 +1,20 @@
 public class Plante:Terrain
 {
-    public string Nom {get; protected set;}
-    public string Nature {get;protected set;}
-    public string SaisonOptimale {get;protected set;}
-    public string TypePref {get; protected set;}
-    public double EspacementEntre2 {get;protected set;}
-    public double PlaceNecessaire {get;protected set;}
-    public double VitessePousse {get;protected set;}
-    public double EauNecessaire {get; protected set;}
-    public double LumiereNecessaire {get; protected set;}
-    public double TemperaturePref {get; protected set;}
-    public string MaladiePotentielle {get; protected set;}
-    public double EsperanceVie {get; protected set;}
-    public double NombreProduction {get; protected set;}
-    public char Visuel {get; set;}
-    public double NiveauHumiditePref{get; protected set;}
+    public string Nom {get; protected set;} // nom de la plante (carotte/cosmos/tournesol...)
+    public string Nature {get;protected set;} // plante vivace, decorative, consommable...
+    public string SaisonOptimale {get;protected set;} //saison optimale pour se developper (ete, automne, hiver, printemps)
+    public string TypePref {get; protected set;} //type de terrain prefere pour se developper
+    public double EspacementEntre2 {get;protected set;} //nombre de parcelles necessaire entre la plante et la suivante
+    public double PlaceNecessaire {get;protected set;} //place necessaire de parcelle pour que la plante se developpe au max
+    public double VitessePousse {get;protected set;} //cm par jour ou semaine que la plante prends avec les meilleures conditions
+    public double EauNecessaire {get; protected set;} //litres d'eau necessaire par jour/semaine pour la plante
+    public double LumiereNecessaire {get; protected set;} //pourcentage de lumiere necessaire optimal
+    public double TemperaturePref {get; protected set;} //temperature prefere de la plante
+    public string MaladiePotentielle {get; protected set;} //maladies potentielles qu'elle peut avoir (probabilité gerer aleatoirement)
+    public double EsperanceVie {get; protected set;} //nombre de semaine ou année de vie de la plante
+    public double NombreProduction {get; protected set;} //nombre de fruit ou fleur par pied qu'elle peut produire au max en 1 saison
+    public char Visuel {get; set;} //emoji de la plante
+    public double NiveauHumiditePref{get; protected set;} //pourcentage d'humidite prefere de la plnate pour se developper au max
 
     public Plante(string Nom, char Nature, string SaisonOptimale, string TypePref, double EspacementEntre2, double PlaceNecessaire,
     double VitessePousse, double EauNecessaire, double LumiereNecessaire, double TemperaturePref, string MaladiePotentielle,
@@ -39,14 +39,17 @@ public class Plante:Terrain
 
     public void RespecterConditions()
     {
-        double PourcentageConditions=0;
+        double PourcentageConditions=0; //pourcentage pour evaluer le respect des condition, agit comme un coeficient de pousse
+        // si le terrain de la plante est le meme que son terrain de predilection, augmenter le pourcentage
         if (TypeTerrain==TypePref)
         {
             PourcentageConditions+=100;
         }
-        double differenceHumidite=NiveauHumidite-NiveauHumiditePref
+        //ajouter la difference de pourcentage entre l'humidité reel et celle desirée
+        double differenceHumidite=NiveauHumidite-NiveauHumiditePref;
         PourcentageConditions-=differenceHumidite;
-        double differenceTemperature=Temperature-TemperaturePref
+        //ajouter la difference de pourcentage entre la temperature reel et celle desirée
+        double differenceTemperature=Temperature-TemperaturePref; //voir comment faire pour que tt soit en %
         PourcentageConditions-=differenceTemperature;
 
 
