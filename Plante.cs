c66cceeec3987c48a9c0b27d572706c98075762e
Emma <eveyrent@ensc.fr>
2025-04-14 16:44:59 +0200

    debut respecter les conditions

--- Plante.cs
+++ Plante.cs
@@ -1,7 +1,7 @@
-public class Plante
+public class Plante:Terrain
 {
     public string Nom {get; protected set;}
-    public char Nature {get;protected set;}
+    public string Nature {get;protected set;}
     public string SaisonOptimale {get;protected set;}
     public string TypePref {get; protected set;}
     public double EspacementEntre2 {get;protected set;}
@@ -9,14 +9,16 @@ public class Plante
     public double VitessePousse {get;protected set;}
     public double EauNecessaire {get; protected set;}
     public double LumiereNecessaire {get; protected set;}
-    public double TempPref {get; protected set;}
+    public double TemperaturePref {get; protected set;}
     public string MaladiePotentielle {get; protected set;}
     public double EsperanceVie {get; protected set;}
     public double NombreProduction {get; protected set;}
+    public char Visuel {get; set;}
+    public double NiveauHumiditePref{get; protected set;}
 
     public Plante(string Nom, char Nature, string SaisonOptimale, string TypePref, double EspacementEntre2, double PlaceNecessaire,
-    double VitessePousse, double EauNecessaire, double LumiereNecessaire, double TempPref, string MaladiePotentielle,
-    double EsperanceVie, double NombreProduction)
+    double VitessePousse, double EauNecessaire, double LumiereNecessaire, double TemperaturePref, string MaladiePotentielle,
+    double EsperanceVie, double NombreProduction, char Visuel, double NiveauHumiditePref)
     {
         this.Nom=Nom;
         this.Nature=Nature;
@@ -27,9 +29,26 @@ public class Plante
         this.VitessePousse=VitessePousse;
         this.EauNecessaire=EauNecessaire;
         this.LumiereNecessaire=LumiereNecessaire;
-        this.TempPref=TempPref;
+        this.TemperaturePref=TemperaturePref;
         this.MaladiePotentielle=MaladiePotentielle;
         this.EsperanceVie=EsperanceVie;
         this.NombreProduction=NombreProduction;
+        this.Visuel=Visuel;
+        this.NiveauHumiditePref=NiveauHumiditePref;
+    }
+
+    public void RespecterConditions()
+    {
+        double PourcentageConditions=0;
+        if (TypeTerrain==TypePref)
+        {
+            PourcentageConditions+=100;
+        }
+        double differenceHumidite=NiveauHumidite-NiveauHumiditePref
+        PourcentageConditions-=differenceHumidite;
+        double differenceTemperature=Temperature-TemperaturePref
+        PourcentageConditions-=differenceTemperature;
+
+
     }
 }
