9e9b2556c4969ef17964a46976d029180eef16e0
ambreprz <aperez008@ensc.fr>
2025-04-14 16:07:15 +0200

    ModifTerrain

--- Terrain.cs
+++ Terrain.cs
@@ -8,26 +8,40 @@ public abstract class Terrain {
     public double PH {get; protected set;} // compris entre 0 et 14 
     public double NiveauSoleil {get ; protected set ;} //Pourcentage ensoleillement entre 0 et 100
     public List<Plante> Plantes {get; protected set;} //accès à la classe plante définié ailleurs
-    
+    public int random Longueur {get; protected set;}
+    public int random Largeur {get; protected set ;}
+
+    public ParcelleTerrain[,] Grille { get; protected set; }
+    public int Largeur { get; protected set; }
+    public int Hauteur { get; protected set; }
+
 protected Terrain(string nom, double surface)
     {
         Nom = nom;
         Surface = surface;
-        Plantes = new List<Plante>();
-        
+        Largeur = largeur;
+        Longueur= longueur;
+        Grille = new CelluleTerrain[largeur, hauteur]; //initialise la grille donc le terrain globalement
+        for (int i = 0; i < largeur; i++)
+        {
+            for (int j = 0; j < hauteur; j++)
+            {
+                Grille[i, j] = new ParcelleTerrain(this);
+            }
+        }
+                    
+    
         //Par défaut
         NiveauHumidite = 50;
         Temperature = 20;
         ExpositionSoleil = 50;
     }
-public virtual bool PlanterUnePlante(Plante.plante)
-    {
-        return Plantes.Add(plante);
-    }
-public virtual bool RetirerPlante(Plante plante)
-    {
-        return Plantes.Remove(plante);
-    }
-
+public virtual bool PlanterUnePlante(Plante plante, int i, int j)
+        {
+            if (i < 0 || i >= Largeur || j < 0 || j >= Hauteur) //vérifie si case dans le terrain, return false si hors des lim
+                return false;
+            
+            
+        }
 }
 
