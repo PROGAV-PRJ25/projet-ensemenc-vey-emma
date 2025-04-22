7fad9fcba56ea3362d0ca641c35157faf2b82739
Emma <eveyrent@ensc.fr>
2025-04-22 11:19:18 +0200

    conditions dev

--- Plante.cs
+++ Plante.cs
@@ -8,7 +8,7 @@ public class Plante:Terrain
     public double PlaceNecessaire {get;protected set;} //place necessaire de parcelle pour que la plante se developpe au max
     public double VitessePousse {get;protected set;} //cm par jour ou semaine que la plante prends avec les meilleures conditions
     public double EauNecessaire {get; protected set;} //litres d'eau necessaire par jour/semaine pour la plante
-    public double LumiereNecessaire {get; protected set;} //pourcentage de lumiere necessaire optimal
+    public double EnsoleillementNecessaire {get; protected set;} //pourcentage de lumiere necessaire optimal
     public double TemperaturePref {get; protected set;} //temperature prefere de la plante
     public string MaladiePotentielle {get; protected set;} //maladies potentielles qu'elle peut avoir (probabilité gerer aleatoirement)
     public double EsperanceVie {get; protected set;} //nombre de semaine ou année de vie de la plante
@@ -40,18 +40,78 @@ public class Plante:Terrain
     public void RespecterConditions()
     {
         double PourcentageConditions=0; //pourcentage pour evaluer le respect des condition, agit comme un coeficient de pousse
+
         // si le terrain de la plante est le meme que son terrain de predilection, augmenter le pourcentage
         if (TypeTerrain==TypePref)
         {
             PourcentageConditions+=100;
         }
+        
         //ajouter la difference de pourcentage entre l'humidité reel et celle desirée
-        double differenceHumidite=NiveauHumidite-NiveauHumiditePref;
+        double differenceHumidite= Math.Abs(NiveauHumidite-NiveauHumiditePref);
         PourcentageConditions-=differenceHumidite;
-        //ajouter la difference de pourcentage entre la temperature reel et celle desirée
-        double differenceTemperature=Temperature-TemperaturePref; //voir comment faire pour que tt soit en %
-        PourcentageConditions-=differenceTemperature;
 
+        //ajouter la difference de pourcentage entre l'ensoleillement reel et celle desirée
+        double differenceEnsoleillement= Math.Abs(EnsoleillementNecessaire-NiveauSoleil); 
+        PourcentageConditions-=differenceEnsoleillement;
 
+        // si la plante n'est pas malade, son coefficient de developpement augmente
+        if (Maladie==0)
+        {
+            PourcentageConditions+=100;
+        }
+
+        // si la temperature est de +/-10°C, le coef de dev diminue de 100%, 
+        //sinon on prends la diff de temp entre la reel et la voulue puis on la multiplie par 10 pour avoir un pourcentage valide et on l'enleve du coef de dev
+        double differenceTemperature = Math.Abs(Temperature-TemperaturePref);
+        if (differenceTemperature<=10)
+        {
+            PourcentageConditions-=100;
+        }
+        else
+        {
+            PourcentageConditions-=(differenceTemperature*10);
+        }
+
+        //compter combien de case auutour de la plantes sont disponibles
+        double espaceAutourDispo = EspaceAutourDispo(i, j);
+        //s'il y a plus de place dispo que necessaire, augmenter le coef de dev de 100%
+        if (espaceAutourDispo==PlaceNecessaire)
+        {
+            PourcentageConditions+=100;
+        }
+        //s'il y a moins de place que necessaire, alors faire le rapport de ce qu'il ya sur ce qu'il faut et le mettre en pourcentage
+        else
+        {
+            PourcentageConditions-= ((espaceAutourDispo/PlaceNecessaire)*100);
+        }
+
+        //diviser le coef de dev par le nombre de conditions pour avoir un resultat entre 0 et 100
+        PourcentageConditions=(PourcentageConditions/6);
+
+    }
+
+    public int EspaceAutourDispo(int i, int j)
+    {
+        double espaceAutourDispo=0;
+
+        // verifier qu'on est bien dans la limite du terrain -> i=colonne et j=ligne
+        if (i < 0 || i >= Largeur || j < 0 || j >= Hauteur)
+        {
+            // parcourir les places autour de la plantes et si elles sont vide (=0) alors rajouter 1 au compteur de case dispo autour
+            for (int m = i - 1; m <= i + 1; m++)
+            {
+                for (int n = i - 1; n <= i + 1; n++)
+                {
+                    if (Grille[m,n]==0)
+                    {
+                        espaceAutourDispo+=1;
+                    }
+                }
+            }
+        }
+
+        //renvoyer le compteur pour le comparer a la valeur voulue
+        return espaceAutourDispo;
     }
 }
